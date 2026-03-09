adacde5c982bda92f2dfbaec9eeb44d01eb4a93b
nivorako <nivo.rakoto@yahoo.fr>
2026-03-09 03:16:21 +0100

    modify welocome text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-      <h1>Hello World</h1>
-      </main>
-    </div>
-  );
+    return (
+        <div className={styles.page}>
+            <main className={styles.main}>
+                <h1>Faly miarahaba antsika rehetra !!!</h1>
+            </main>
+        </div>
+    );
 }
